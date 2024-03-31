ce55b0ea3be6a8ac0f43b12fd42d1c281da88e26
AleksandarGazikalovic <aleksandargazikalovic@gmail.com>
2024-03-31 19:37:55 +0200

    Changed userId from string to objectid in post model

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema(
   {
     userId: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
     vehicleId: {
